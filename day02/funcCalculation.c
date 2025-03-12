60d0f8c46cd992422e441aaf71c58ff91efb84ea
emilianolee <yechan5172@daum.net>
2025-03-12 18:13:25 +0900

    2일차 중간 중간 커밋

--- day02/funcCalculation.c
+++ day02/funcCalculation.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
 
-float func() {
-	printf("계산기 프로그램입니다.\n");
+int plus(int n1, int n2);
+int minus(int n1, int n2);
+int multiply(int n1, int n2);
+double divide(int n1, int n2);
 
+int main()
+{
 	int n1, n2;
 	char tool;
 	printf("계산식을 입력하세요.(예: 3 + 8)\n");
 	scanf("%d %c %d", &n1, &tool, &n2);
-	
-	if (tool == '+') { printf("%d", n1 + n2); }
-	else if (tool == '-') { printf("%d", n1 - n2); }
-	else if (tool == '*') { printf("%d", n1 * n2); }
-	else if (tool == '/') { printf("%.2lf", n1 / n2); }
-	else { printf("잘못입력하셨습니다."); }
+
+	if (tool == '+') { 
+		printf("%d + %d = %d\n", n1, n2, plus(n1, n2)); 
+	}
+	else if (tool == '-') { 
+		printf("%d - %d = %d\n", n1, n2, minus(n1, n2)); 
+	}
+	else if (tool == '*') { 
+		printf("%d * %d = %d\n", n1, n2, multiply(n1, n2)); 
+	}
+	else if (tool == '/') { 
+		printf("%d / %d = %lf\n", n1, n2, divide(n1, n2)); 
+	}
+	else { 
+		printf("잘못입력하셨습니다.\n"); 
+	}
 }
 
-int main()
-{
-	func();
-	return 0;
+int plus(int n1, int n2) {
+	return n1 + n2;
+}
+int minus(int n1, int n2) {
+	return n1 - n2;
+}
+int multiply(int n1, int n2) {
+	return n1 * n2;
+}
+double divide(int n1, int n2) {
+	return double(n1) / double(n2);
 }
