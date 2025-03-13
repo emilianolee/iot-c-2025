f04032bb48359ed7792b0d4ea8ff50c1f26132a8
emilianolee <yechan5172@daum.net>
2025-03-13 17:28:04 +0900

    3일차 정말 최종입니다

--- dia 02/func.c
+++ dia 02/func.c
@@ -36,20 +36,20 @@
 /*
 	함수: 입력과 출력이 있는 함수
 */
-//#include <stdio.h>
-//
-//int func(int a, int b) {
-//	int res = a + b;
-//	return res;
-//}
-//
-//int main()
-//{
-//	int result = func(10, 20);
-//	printf("호출 후 리턴된 값: %d", result);
-//
-//	return 0;
-//}
+#include <stdio.h>
+
+int func(int a, int b) {
+	int res = a + b;
+	return res;
+}
+
+int main()
+{
+	int result = func(10, 20);
+	printf("호출 후 리턴된 값: %d", result);
+
+	return 0;
+}
 
 /*
 	함수 원형
